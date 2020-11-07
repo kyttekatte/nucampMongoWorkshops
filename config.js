e6185a736ad0ca8f4b7a002c937eb495ce16e617
Kytte Burke <gameorg@gmail.com>
2020-11-07 10:10:47 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '951886265335081',
+        clientSecret: 'daa0f7a0b38ea511d064a55e64d8169f'
+    }
 }
